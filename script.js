407248fa83fc4ed2a65993d4ab99de1ef2d78313
kjiang123 <114095151+kjiang123@users.noreply.github.com>
2023-10-25 15:01:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function swapHydro(){
+    const image = document.getElementById(`robot`);
+    image.src = `Images/Robot_h.png`;
+}
+
+function swapSolar(){
+    const image = document.getElementById(`robot`);
+    image.src = `Images/Robot_s.png`;
+}
+
+function swapNuclear(){
+    const image = document.getElementById(`robot`);
+    image.src = `Images/Robot_n.png`;
+}
+
+function scannerON(){
+    
+}
+
+
