a485032e81e7b8fc5d927d6086b9415f77702c28
kjiang123 <114095151+kjiang123@users.noreply.github.com>
2023-10-31 15:29:38 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function swapHydro(){
     image.src = `Images/Robot_h.png`;
     type = "Hydro";
     document.getElementById('typeText').innerHTML = type;
+    document.getElementById("defSystem").disabled = false;
+    document.getElementById("scanner").disabled=false; 
+    document.getElementById("defSystem").checked = false;
+    document.getElementById("scanner").checked=false; 
 }
 
 function swapSolar(){
@@ -13,6 +17,10 @@ function swapSolar(){
     image.src = `Images/Robot_s.png`;
     type = "Solar";
     document.getElementById('typeText').innerHTML = type;
+    document.getElementById("defSystem").disabled = false;
+    document.getElementById("scanner").disabled=false; 
+    document.getElementById("defSystem").checked = false;
+    document.getElementById("scanner").checked = false;
 }
 
 function swapNuclear(){
@@ -20,6 +28,10 @@ function swapNuclear(){
     image.src = `Images/Robot_n.png`;
     type = "Nuclear";
     document.getElementById('typeText').innerHTML = type;
+    document.getElementById("defSystem").disabled = false;
+    document.getElementById("scanner").disabled=false; 
+    document.getElementById("defSystem").checked = false;
+    document.getElementById("scanner").checked = false;
 }
 
 function scannerON(){
@@ -81,6 +93,44 @@ function integrityChange()
     }
 }
 
+function finalize(){
+    document.getElementById("hydro").disabled = true;
+    document.getElementById("solar").disabled = true;
+    document.getElementById("nuclear").disabled = true;
+    document.getElementById("scanner").disabled = true;
+    document.getElementById("defSystem").disabled = true;
+    document.getElementById("power").disabled = true;
+    document.getElementById("speed").disabled = true;
+    document.getElementById("integrity").disabled = true;
+}
 
+function findMaxPower(){
+    if (type == "Hydro"){
+        return 8;
+    } else if (type == "Solar"){
+        return 10;
+    } else {
+        return 12;
+    }
+    
+}
 
+function findMaxSpeed(){
+    if (type == "Hydro"){
+        return 12;
+    } else if (type == "Solar"){
+        return 10;
+    } else {
+        return 14;
+    }
+}
 
+function findMaxSI(){
+    if (type == "Hydro"){
+        return 10;
+    } else if (type == "Solar"){
+        return 10;
+    } else {
+        return 4;
+    }
+}
